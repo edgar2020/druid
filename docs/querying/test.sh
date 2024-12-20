6a8f129a741b5e5f49bcac2455a9ee7062298a7a
Victoria Lim <vtlim@users.noreply.github.com>
2024-12-20 14:10:00 -0800

    Delete docs/querying/test.sh

--- docs/querying/test.sh
+++
@@ -1,6 +0,0 @@
-curl "http://localhost:8888/druid/v2/sql" \
---header 'Content-Type: application/json' \
---data '{
-    "query": "SELECT \"arrayDouble\" FROM \"array_example\" WHERE ARRAY_CONTAINS(\"arrayDouble\", ?)",
-    "parameters": [{"type": "ARRAY", "value": [999.0, null, 5.5]}] 
-}'
